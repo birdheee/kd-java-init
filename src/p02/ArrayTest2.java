88719bd029cc16e1ef037e91d9975bd81ebdee1d
rkdghks <olaffub2@gmail.com>
2023-06-27 13:59:00 +0900

    array test

---
+++ src/p02/ArrayTest2.java
@@ -0,0 +1,23 @@
+package p02;
+
+public class ArrayTest2 {
+
+	public static void main(String[] args) {
+		String[] strs = new String[10];
+		/*
+		 * 0 -> 2 1
+		 * 1 -> 4 2
+		 * 2 -> 6 3
+		 * 3 -> 8 4
+		 * ....
+		 * 9 -> 20 
+		 */
+		for(int i=0;i<10;i++) {
+			strs[i] = (i+1) * 2 + "";
+		}
+		
+		for(int i=0;i<10;i++) {
+			System.out.println("strs[" + i + "] = " + strs[i]);
+		}
+	}
+}
